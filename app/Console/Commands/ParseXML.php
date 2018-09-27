ef62bf1adbebb6377eb626ebdbbf90439019d563
Andrejs Gubars <andrejs.gubars@caliberi.com>
2018-09-27 16:51:34 +0100

    closes #6

--- app/Console/Commands/ParseXML.php
+++ app/Console/Commands/ParseXML.php
@@ -65,7 +65,7 @@ class ParseXML extends Command
             ]);
 
             $vehicleDetails = new VehicleDetails([
-                'licence_plate' => $vehicle->license_plate,
+                'license_plate' => $vehicle->license_plate,
                 'userdetails_id' => $userDetails->id,
                 'make' => $vehicle->attributes()->manufacturer,
                 'model' => $vehicle->attributes()->model,
